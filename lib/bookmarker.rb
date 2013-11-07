971f45e91438bd42036ccb3b6e6ca0c24100961c
Abdi Ali <abdirazak.ali.12@live.com>
2013-11-07 16:35:44 +0000

    updated lib files tag

--- lib/bookmarker.rb
+++ lib/bookmarker.rb
@@ -4,6 +4,7 @@ require 'data_mapper'
 env = ENV["RACK_ENV"] || "development"
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 require_relative 'link'
+require_relative 'tag'
 # After declaring your models, you should finalise them
 DataMapper.finalize
 
@@ -19,18 +20,37 @@ class Bookmarker < Sinatra::Base
 	end
 
 	get '/links' do
-		@links = Link.all
+		# @links = Link.all
 		erb :links
 	end
 
 	post '/links' do 
 		url = params["url"]
-		title = params ["title"]
-		description = params ["description"]
-		Link.create(:url => url, :title => title, :description => description)
+		title = params["title"]
+		description = params["description"]
+		tags = params["tags"].split(' ').map do |tag|
+			Tag.first_or_create(:text => tag)
+		end
+		Link.create(:url => url, :title => title, :description => description, :tags => tags)
 		redirect to('/')
 	end
 
+	get '/tags' do 
+		erb :tag_search
+	end
+
+	get '/tags/:text' do |text|
+		puts text
+ 	 	tag = Tag.first(:text => params[:text])
+   		@links = tag ? tag.links : []
+ 	 	erb :index
+ 	end
+
+	post '/tags' do
+    	tag = params["tags"]
+    	tag_search = '/tags/' + tag
+    	redirect to(tag_search)
+   end
 
 	get '/login' do
 		@login_page = true
